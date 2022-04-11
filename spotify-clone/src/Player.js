77452049c68e87e807b64faaed1c94275a81cf20
boogerjosh <joshsudarso19@gmail.com>
2022-04-11 16:15:46 +0700

    new commit spotify-clone

--- spotify-clone/src/Player.js
+++ spotify-clone/src/Player.js
@@ -7,11 +7,11 @@ import Footer from './Footer';
 function Player({ spotify }) {
   return (
     <div className='player'>
-      <div className='player_body'>
+      <div className='player__body'>
         <Sidebar />
         <Body spotify={spotify} />
       </div>
-      <Footer />
+      <Footer spotify={spotify} />
     </div>
   );
 };
--- spotify-clone/src/Sidebar.js
+++ spotify-clone/src/Sidebar.js
@@ -8,13 +8,13 @@ import { useDataLayerValue } from './DataLayer';
 
 function Sidebar() {
   const [{ playlists }, dispatch] = useDataLayerValue();
-  console.log(playlists, '===')
   return (
     <div className='sidebar'>
       <img
-        className='sidebar_logo'
-        src='https://www.birnoktabir.com/wp-content/uploads/2017/04/spotify-logo-070417.jpg'
-        alt='' />
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
       <SidebarOption Icon={HomeIcon} title='Home' />
       <SidebarOption Icon={SearchIcon} title='Search' />
       <SidebarOption Icon={LibraryMusicIcon} title='Library' />
